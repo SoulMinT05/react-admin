94129ed808e1a591df8128c0731c1ec2431f29d7
tamsoul <tamsoul>
2024-06-10 16:36:32 +0700

    navbar design

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,27 @@
 import './navbar.scss';
 
 function Navbar() {
-    return <div>Navbar</div>;
+    return (
+        <div className="navbar">
+            <div className="logo">
+                <img src="logo.svg" alt="" />
+                <span>minhtam</span>
+            </div>
+            <div className="icons">
+                <img src="/search.svg" alt="" className="icon" />
+                <img src="/app.svg" alt="" className="icon" />
+                <div className="notification">
+                    <img src="/notifications.svg" alt="" />
+                    <span>1</span>
+                </div>
+                <div className="user">
+                    <img src="/user-admin.jpeg" alt="" />
+                    <span>Tam</span>
+                </div>
+                <img src="/settings.svg" alt="" className="icon" />
+            </div>
+        </div>
+    );
 }
 
 export default Navbar;
